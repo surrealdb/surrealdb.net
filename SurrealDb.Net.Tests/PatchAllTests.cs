e02e9502d791bd5ec87e15f935112ba51eb21dc1
Odonno <bottiau.david@laposte.net>
2023-11-30 18:25:16 +0100

    ⚡️ replace localhost by local ip

--- SurrealDb.Net.Tests/PatchAllTests.cs
+++ SurrealDb.Net.Tests/PatchAllTests.cs
@@ -7,8 +7,8 @@ namespace SurrealDb.Net.Tests;
 public class PatchAllTests
 {
     [Theory]
-    [InlineData("http://localhost:8000")]
-    [InlineData("ws://localhost:8000/rpc")]
+    [InlineData("http://127.0.0.1:8000")]
+    [InlineData("ws://127.0.0.1:8000/rpc")]
     public async Task ShouldPatchAllRecords(string url)
     {
         IEnumerable<Post>? list = null;
--- SurrealDb.Net.Tests/PatchTests.cs
+++ SurrealDb.Net.Tests/PatchTests.cs
@@ -7,8 +7,8 @@ namespace SurrealDb.Net.Tests;
 public class PatchTests
 {
     [Theory]
-    [InlineData("http://localhost:8000")]
-    [InlineData("ws://localhost:8000/rpc")]
+    [InlineData("http://127.0.0.1:8000")]
+    [InlineData("ws://127.0.0.1:8000/rpc")]
     public async Task ShouldPatchExistingPost(string url)
     {
         IEnumerable<Post>? list = null;
