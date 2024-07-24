f2ce8066441751b9c317222c1cbaadbdb5e6d641
Odonno <bottiau.david@laposte.net>
2024-07-24 11:25:02 +0200

    ✨ implement IComparable for StringRecordId

--- SurrealDb.Net/Models/StringRecordId.cs
+++ SurrealDb.Net/Models/StringRecordId.cs
@@ -12,7 +12,9 @@
 /// This library will never be able to deserialize a <see cref="StringRecordId"/> into a <see cref="Thing"/>.
 /// As such, please consider using <see cref="Thing"/> or one of its inherited types for these scenarii.
 /// </remarks>
-public readonly partial struct StringRecordId : IEquatable<StringRecordId>
+public readonly partial struct StringRecordId
+    : IEquatable<StringRecordId>,
+        IComparable<StringRecordId>
 {
     /// <summary>
     /// The value representing the Record ID as a single string, e.g.
@@ -23,6 +25,7 @@ public readonly partial struct StringRecordId : IEquatable<StringRecordId>
     /// <summary>
     /// Instanciate a new <see cref="StringRecordId"/> that will contain a Record ID representation as a single string.
     /// </summary>
+    /// <exception cref="InvalidOperationException">A value is required</exception>
     public StringRecordId()
     {
         throw new InvalidOperationException("A value is required");
@@ -53,5 +56,10 @@ public readonly partial struct StringRecordId : IEquatable<StringRecordId>
     public bool Equals(StringRecordId other)
     {
         return Value == other.Value;
+    }
+
+    public int CompareTo(StringRecordId other)
+    {
+        return Value.CompareTo(other.Value);
     }
 }
