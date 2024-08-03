e78d8943a5e2ec72151c2e9f3fb5f689417097e0
Odonno <bottiau.david@laposte.net>
2024-08-03 11:50:45 +0200

    ⚡️ use string.Create to generate random string id

--- SurrealDb.Net.LocalBenchmarks/RandomIdBenchmark.cs
+++ SurrealDb.Net.LocalBenchmarks/RandomIdBenchmark.cs
@@ -11,10 +11,10 @@ namespace SurrealDb.Net.LocalBenchmarks;
 /// </summary>
 public class RandomIdBenchmark
 {
-    [Params(1, 20)]
+    [Params(1, 20, 100)]
     public int Iterations;
 
-    private string[] _results = new string[20];
+    private string[] _results = new string[100];
 
     [Benchmark(Baseline = true)]
     public string[] FromRandomInt()
@@ -97,6 +97,37 @@ public class RandomIdBenchmark
         for (int i = 0; i < Iterations; i++)
         {
             _results[i] = Ulid.NewUlid().ToString()[..8];
+        }
+        return _results;
+    }
+
+    [Benchmark]
+    public string[] FromStringCreate()
+    {
+        const string encodeString = "0123456789abcdef";
+        int encodeMaxIndex = encodeString.Length - 1;
+
+        char[] encode32Chars = encodeString.ToCharArray();
+
+        for (int i = 0; i < Iterations; i++)
+        {
+            const int length = 8;
+
+            _results[i] = string.Create(
+                length,
+                Random.Shared.NextInt64(),
+                (buffer, value) =>
+                {
+                    buffer[7] = encode32Chars[value & encodeMaxIndex];
+                    buffer[6] = encode32Chars[(value >> 5) & encodeMaxIndex];
+                    buffer[5] = encode32Chars[(value >> 10) & encodeMaxIndex];
+                    buffer[4] = encode32Chars[(value >> 15) & encodeMaxIndex];
+                    buffer[3] = encode32Chars[(value >> 20) & encodeMaxIndex];
+                    buffer[2] = encode32Chars[(value >> 25) & encodeMaxIndex];
+                    buffer[1] = encode32Chars[(value >> 30) & encodeMaxIndex];
+                    buffer[0] = encode32Chars[(value >> 35) & encodeMaxIndex];
+                }
+            );
         }
         return _results;
     }
