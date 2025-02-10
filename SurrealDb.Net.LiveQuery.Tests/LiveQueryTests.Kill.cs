3974bead6b4d6996cd284912f86b9620a0316f72
Emmanuel Keller <emmanuel.keller@surrealdb.com>
2025-02-10 11:35:22 +0000

    Wip

--- SurrealDb.Net.LiveQuery.Tests/LiveQueryTests.Kill.cs
+++ SurrealDb.Net.LiveQuery.Tests/LiveQueryTests.Kill.cs
@@ -97,12 +97,12 @@ public class KillLiveQueryTests
 
         await manuallyKillLiveQueryFunc.Should().NotThrowAsync();
 
-        Func<SurrealDbException, bool> validErrorMessage = ex =>
+        Expression<Func<SurrealDbException, bool>> validErrorMessage = ex =>
             ex.Message.Contains(
                 "There was a problem with the database: Can not execute KILL statement using id"
             )
             && (
-                ex.Message.Contains(liveQueryUuid)
+                ex.Message.Contains(liveQueryUuid.ToString())
                 || ex.Message.Contains("KILL statement uuid did not exist")
             );
 
