f666aaea3404df67bc03baafb1a2d23e3248e4bf
Odonno <bottiau.david@laposte.net>
2026-03-25 13:49:07 +0100

    ✅ rewrite sandbox tests

--- SurrealDb.Net.Tests/Queryable/DebugQueryableTests.cs
+++ SurrealDb.Net.Tests/Queryable/DebugQueryableTests.cs
@@ -5,19 +5,52 @@
 /// </summary>
 public class DebugQueryableTests : BaseQueryableTests
 {
+    /// <summary>
+    /// A sample test to try the query generation.
+    /// </summary>
     [Test]
     [Skip("Sandbox")]
-    public void Debug()
+    public void QueryGenerator()
     {
-        //string query = ToSurql(Users.Where(u => u.Age > Math.PI));
         string query = ToSurql(Posts.Where(p => p.CreatedAt > DateTime.Now));
 
         query
             .Should()
             .Be(
                 """
-                SELECT * FROM post WHERE Title == "Title 1"
+                SELECT content, created_at, id, status, title FROM post WHERE created_at > time::now()
                 """
             );
+    }
+
+    /// <summary>
+    /// A sample test to try a query against a memory instance.
+    /// </summary>
+    [Test]
+    [WebsocketConnectionStringFixtureGenerator]
+    [Skip("Sandbox")]
+    public async Task Sandbox(string connectionString)
+    {
+        IEnumerable<Post>? result = null;
+
+        Func<Task> func = async () =>
+        {
+            await using var surrealDbClientGenerator = new SurrealDbClientGenerator();
+            var dbInfo = surrealDbClientGenerator.GenerateDatabaseInfo();
+
+            await using var client = surrealDbClientGenerator.Create(connectionString);
+            await client.Use(dbInfo.Namespace, dbInfo.Database);
+
+            await client.ApplySchemaAsync(SurrealSchemaFile.Post);
+
+            result = await client
+                .Select<Post>("post")
+                .Where(p => p.CreatedAt > DateTime.Now)
+                .ToListAsync();
+        };
+
+        await func.Should().NotThrowAsync();
+
+        result.Should().NotBeNull().And.HaveCount(0);
     }
 }
