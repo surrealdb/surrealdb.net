3e0d2826894e15562183420902d7fb331ccfd1cf
Odonno <bottiau.david@laposte.net>
2026-05-11 11:40:29 +0200

    ✅ simplify test fixture

---
+++ SurrealDb.Net.Tests.Fixtures/DatabaseInfoFaker.cs
@@ -0,0 +1,12 @@
+﻿using Bogus;
+
+namespace SurrealDb.Net.Tests.Fixtures;
+
+internal sealed class DatabaseInfoFaker : Faker<DatabaseInfo>
+{
+    public DatabaseInfoFaker()
+    {
+        RuleFor(o => o.Namespace, f => f.Random.AlphaNumeric(40));
+        RuleFor(o => o.Database, f => f.Random.AlphaNumeric(40));
+    }
+}
